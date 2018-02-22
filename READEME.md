4d1209ebe70227e0f2fd16f9d06d439b776f6dd7
Dan Villa <dvilla@collineargroup.com>
2018-02-21 16:01:13 -0800

    Initial commit

---
+++ READEME.md
@@ -0,0 +1,6 @@
+# Documenting Restify with Swagger
+
+Inspired by [a Scotch.io tutorial](https://scotch.io/tutorials/document-your-already-existing-apis-with-swagger).
+
+We'll start with an existing RESTful API that manages League of Legends Champions. We'll use Swagger to first document the existing API and second to provide some sweet UI tools to view our API.
+
---
+++ index.js
@@ -0,0 +1,94 @@
+const restify = require("restify");
+const corsMw = require("restify-cors-middleware");
+
+// Create the server
+const server = restify.createServer({
+  name: "carl-persistence-server"
+});
+
+// Mock DB Setup
+let NEXT_ID = 0;
+const getNextId = () => `${++NEXT_ID}`;
+const db = {
+  champions: [
+    {
+      id: getNextId(),
+      name: "Alistar",
+      roles: ["Support", "Fighter"]
+    }
+  ]
+};
+
+server.use(restify.plugins.bodyParser());
+server.use(restify.plugins.queryParser());
+
+// Example with query params
+server.get("/champion", (req, res, next) => {
+  let champs = [];
+  if (req.query.name) {
+    champs = db.champions.filter(el => el.name.match(req.query.name));
+  } else {
+    // TODO: deep copy
+    champs = db.champions.map(el => Object.assign({}, el));
+  }
+  // TODO: not going to worry about error handling for this tutorial.
+  res.send(200, champs);
+  next();
+});
+
+// Standard rest verbs
+server.post("/champion", (req, res, next) => {
+  if (req.body) {
+    db.champions.push({
+      id: getNextId(),
+      name: body.name,
+      roles: body.roles
+    });
+  }
+  res.send(200);
+  next();
+});
+// Example with route params
+server.get("/champion/:id", (req, res, next) => {
+  let champ = {};
+  if (req.params && req.params.id) {
+    let index = db.champions.findIndex(el => el.id === req.params.id);
+    if (index !== -1) {
+      champ = db.champions[index];
+    }
+  }
+  res.send(200, champ);
+  next();
+});
+server.put("/champion/:id", (req, res, next) => {
+  if (req.params && req.params.id && req.body) {
+    let index = db.champions.findIndex(el => el.id === req.params.id);
+    if (index !== -1) {
+      db.champions.splice(index, 1, {
+        name: req.body.name,
+        roles: req.body.roles
+      });
+    }
+  }
+  res.send(200);
+  next();
+});
+server.del("/champion/:id", (req, res, next) => {
+  if (req.params && req.params.id) {
+    let index = db.champions.findIndex(el => el.id === req.params.id);
+    if (index !== -1) {
+      db.champions.splice(index, 1);
+    }
+  }
+  res.send(200);
+  next();
+});
+
+server.on("error", function(err) {
+  console.error("Server error: ", err);
+});
+
+const port = 9005;
+server.listen(port, function() {
+  console.log(`App listening at http://localhost:${port}`);
+});
